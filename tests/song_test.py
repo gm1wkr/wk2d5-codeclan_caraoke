adb19bea7b1caeb6ed91428eea892805c787732a
GM1WKR <gm1wkr@gmail.com>
2022-05-27 22:42:59 +0100

    test_song_has_play_count

--- tests/song_test.py
+++ tests/song_test.py
@@ -6,4 +6,17 @@ import unittest
 from src.song import Song
 
 class TestSong(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.song_1 = Song("Time Warp", "Richard O'Brian")
+        self.song_2 = Song("We are the Champions", "Queen")
+        self.song_3 = Song("Canyons of My Mind", "Bonzo Dog Doo Dah Band")
+
+    def test_song_has_name(self):
+        self.assertEqual("Time Warp", self.song_1.name)
+
+    def test_song_has_artist(self):
+        self.assertEqual("Richard O'Brian", self.song_1.artist)
+
+    def test_song_has_playcount(self):
+        self.song_2.play_count = 10
+        self.assertEqual(10, self.song_2.play_count)
